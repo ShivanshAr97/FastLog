b716a1867e343faa14320630e820f86fed1c5cf8
ShivanshAr97 <shivansh.arora973@gmail.com>
2024-05-20 19:01:16 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000/api/',
+        target: 'https://loginfinal-zuam.onrender.com/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
